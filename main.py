c47d629f195f8ab4e7043b558615245180317e54
matete007 <maria.tereza.medeiros@hotmail.com>
2025-11-27 23:41:46 -0300

    Conserto de erros e implementação de plataforma móvel

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pygame import Rect
 import pgzero
 import pgzrun
-from pgzero.builtins import Actor, keyboard, clock
+import math
+from pgzero.builtins import Actor, keyboard
 
 TITLE = 'THE MAZE RUNNER GAME'
 WIDTH = 675
@@ -18,23 +19,57 @@ left_ground_width = 106
 right_ground_height = 117
 right_ground_width = 116
 
-# x, y, platform height, platform width    --mas por algum motivo height e width estão trocados na hora de fazer o rect
+#PLATFORMS5
+# x, y, platform width, platform height
 left_ground = Rect(0, 290, left_ground_width, left_ground_height)
 right_ground = Rect(558, 305, right_ground_width, right_ground_height)
+
+p1 = Actor("platafforms2.png")
+p1.topleft = (158, 293)
+p1Rect = Rect(p1.topleft[0], p1.topleft[1], 24, 24)
+p2 = Actor("platafforms2.png")
+p2.topleft = (222, 226)
+p2Rect = Rect(p2.topleft[0], p2.topleft[1], 24, 24)
+p3 = Actor("platafforms2.png")
+p3.topleft = (158, 172)
+p3Rect = Rect(p3.topleft[0], p3.topleft[1], 24, 24)
+p4 = Actor("platafforms2.png")
+p4.topleft = (235, 111)
+p4Rect = Rect(p4.topleft[0], p4.topleft[1], 24, 24)
+
+#MOVING PLATFORM
+movingP = Actor("platafforms3.png")
+movingP.topleft = (30, 74)
+movingPRect = Rect(movingP.topleft[0], movingP.topleft[1], 33, 18)
+PLATFORM_SPEED = 1
+
+moving_platform_data = {
+    'visual': movingP,
+    'rect': movingPRect,
+    'vx': PLATFORM_SPEED,
+    'limit_left': 30,
+    'limit_right': 130
+}
+
+#KEY
+key = Actor("key0.png")
+key.topleft = (movingP.topleft[0] + 5, movingP.topleft[1] - key.height)
+
 
 #PLAYER ANIMATIONS
 player_walk = ['walking-thomas1.png', 'walking-thomas0.png']
 player_idle = ['stopped-thomas0.png', 'stopped-thomas1.png']
-player_breath = ['breathing-thomas0.png', 'breathing-thomas1.png']
-player_jump = ['jumping-thomas0.png', 'jumping-thomas1.png', 'jumping-thomas2.png', 'jumping-thomas3.png', 'jumping-thomas4.png', 'jumping-thomas5.png', 'jumping-thomas6.png', 'jumping-thomas7.png']
+player_jump = ['jumping-thomas1.png', 'jumping-thomas2.png', 'jumping-thomas3.png', 'jumping-thomas4.png', 'jumping-thomas5.png', 'jumping-thomas6.png', 'jumping-thomas7.png']
 
 player = Actor(player_idle[0])
 player.pos = (50, 311 - player.height * 1.5)
-player.height = 65
-player.width = 25
 player.x_previous = player.x
 
-vellocityy = 0
+vellocityy = 1.0
+
+def on_mouse_move(pos):
+    x, y = pos
+    print(f"Mouse X: {x}, Y: {y}")
 
 def update_player(keyboard):
     if keyboard.left:
@@ -42,7 +77,20 @@ def update_player(keyboard):
     if keyboard.right:
         player.x += 3
 
-platforms = [left_ground, right_ground]
+platforms = [left_ground, right_ground, p1Rect, p2Rect, p3Rect, p4Rect, moving_platform_data]
+platform_visuals = [p1, p2, p3, p4, movingP]
+
+def update_platforms():
+    for pt in platforms:
+        if isinstance(pt, dict) and 'vx' in pt:
+            p_rect = pt['rect']
+            p_visual = pt['visual']
+            p_rect.x += pt['vx']
+            if p_rect.right > pt['limit_right']:
+                pt['vx'] = -PLATFORM_SPEED
+            elif p_rect.left < pt['limit_left']:
+                pt['vx'] = PLATFORM_SPEED
+            p_visual.topleft = p_rect.topleft
 
 def set_image_midbottom(image):
     mb = player.midbottom
@@ -58,6 +106,7 @@ current_animation = "idle"
 animation_delay = 0.25
 animation_timer = 0
 index = 0
+MAX_DT = 0.05
 
 def animate(dt, animation):
     global index, animation_timer, current_animation
@@ -82,19 +131,35 @@ on_ground = False
 def update(dt):
     global vellocityy, on_ground
     on_ground = False
+    update_platforms()
+
+    player.y_previous = player.y 
+    player.bottom_previous = player.bottom
+
+    if dt > MAX_DT:
+        dt = MAX_DT
+
 
     if not on_ground:
         vellocityy += GRAVITY * dt
     player.y += vellocityy
 
     for p in platforms:
-        if player.colliderect(p):
+        if isinstance(p, dict):
+            p_rect = p['rect']
+        else:
+            p_rect = p
+        if player.colliderect(p_rect):
             if vellocityy > 0:
-                overlap_y = player.bottom - p.top
-                if overlap_y < COLLISION_TOLERANCE:
-                    player.bottom = p.top
+                if player.bottom_previous <= p_rect.top:
+                    player.bottom = p_rect.top
                     vellocityy = 0
                     on_ground = True
+                    if on_ground and isinstance(p, dict) and 'vx' in p:
+                        player.x += p['vx']
+            elif vellocityy < 0:
+                if player.y_previous >= p_rect.bottom: 
+                     vellocityy = 0
 
     if keyboard.up and on_ground:
         vellocityy = JUMP_FORCE
@@ -124,6 +189,9 @@ def update(dt):
 def draw():
     screen.clear()
     screen.blit('background_', (0,0))
+    for visual in platform_visuals:
+        visual.draw()
     player.draw()
+
 
 pgzrun.go()
